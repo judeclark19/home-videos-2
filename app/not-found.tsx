cbad953dfa53526d73c98c3d56b90367894ed432
judeclark19 <judeclark2019@gmail.com>
2025-03-05 12:49:04 -0500

    centering the 'not found' ui

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,37 @@
 "use client";
 
-import { useRouter } from 'next/navigation'
-import React from 'react'
+import { useRouter } from "next/navigation";
+import React from "react";
 
 function NotFound() {
-
-    const router = useRouter()
-    return (
-        <div>
-            <h1>Page not found</h1>
-            <button onClick={() => {
-                router.back()
-            }}>Go Back</button>
-        </div>
-    )
+  const router = useRouter();
+  return (
+    <div
+      style={{
+        border: "1px solid red",
+        display: "flex",
+        flexDirection: "column"
+      }}
+    >
+      <h1
+        style={{
+          textAlign: "center"
+        }}
+      >
+        Page not found
+      </h1>
+      <button
+        style={{
+          margin: "auto"
+        }}
+        onClick={() => {
+          router.back();
+        }}
+      >
+        Go Back
+      </button>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
